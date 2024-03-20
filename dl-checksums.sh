54beebc5d20d3996f8c58513b929bd7f787c1dbd
Andrew Rothstein <andrew.rothstein@gmail.com>
2024-03-20 16:22:00 +0000

    karmadactl:1.9.0

---
+++ dl-checksums.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+set -e
+
+DIR=~/Downloads
+MIRROR=https://github.com/karmada-io/karmada/releases/download
+APP=karmadactl
+
+dl()
+{
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local archive_type=${4:-tgz}
+    local platform="${os}-${arch}"
+    # https://github.com/karmada-io/karmada/releases/download/v1.9.0/karmadactl-linux-arm64.tgz.sha256
+    local url=$MIRROR/v$ver/${APP}-${platform}.${archive_type}.sha256
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(curl -sSLf $url | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver darwin amd64
+    dl $ver darwin arm64
+    dl $ver linux amd64
+    dl $ver linux arm64
+}
+
+dl_ver ${1:-1.9.0}
